504e62cbb7040405644e74368223cf860de44b29
Gabrielle Castilho <gabriellecastilho@gmail.com>
2023-11-18 20:34:46 +0100

    testing small changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,34 @@
-<!-- modify this form HTML and place wherever you want your form -->
-<form
-  action="https://formspree.io/f/mzblqken"
-  method="POST"
->
-  <label>
-    Your email:
-    <input type="email" name="email">
-  </label>
-  <label>
-    Your message:
-    <textarea name="message"></textarea>
-  </label>
-  <!-- your other form fields go here -->
-  <button type="submit">Send</button>
-</form>
+<?php
+$email_address = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !$email_address)
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+if ($email_address === FALSE) {
+    echo 'Invalid email';
+    exit(1);
+}
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+if (empty($_POST['_gotcha'])) { // If hidden field was filled out (by spambots) don't send!
+    // Create the email and send the message
+    $to = 'gabriellecastilho@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $email_subject = "Website Contact Form:  $name";
+    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+    $headers = "From: noreply@gabriellecastilho.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+    return true;
+}
+echo "Gotcha, spambot!";
+return false;
+?>
